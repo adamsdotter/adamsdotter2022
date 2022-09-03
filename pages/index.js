de23fb87542c79f89fa0c5af6a87396474442c8a
Patricia Adamsson <hello@dev-cat.com>
2022-09-03 18:33:51 +0200

    Add intro section

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,20 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Hi! I am Patricia.</h1>
+        <section className={styles.section}>
+          <p>
+            I'm a senior front-end developer. I've been working professionally in the industry as a developer for more than 14 years and full-time as a <a href="https://dev-cat.com/">freelancer</a> since 2018.
+          </p>
+          <p>
+            My biggest passion lies within Front-end techniques such as JavaScript, CSS and HTML.
+            I focus on semantically correct responsive websites which are available for everyone, no matter device or disability.
+            The past years I've been working mainly with React and Vue.
+          </p>
+          <p>
+            Since 2014 I've been working remotely and have the ambition to continue this way.
+            I am open to fun projects world wide as long as I can work from home.
+          </p>
+        </section>
       </main>
     </div>
   )
